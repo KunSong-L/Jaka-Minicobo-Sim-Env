0c329597c1788d88acbb01cfa3dcc94d1d600faa
Song Kun <coldtea@sjtu.edu.cn>
2023-04-12 20:39:36 +0800

    修复bug

---
+++ jaka_sim_env/scripts/save_fig.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+#!coding=utf-8
+ 
+import rospy
+import numpy as np
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+import cv2
+
+save_path = "../fig/"
+def callback(data):
+    # define picture to_down' coefficient of ratio
+    scaling_factor = 0.5
+    global count,bridge
+    count = count + 1
+    if count == 1:
+        count = 0
+        cv_img = bridge.imgmsg_to_cv2(data, "bgr8")
+        timestr = "%.6f" %  data.header.stamp.to_sec()
+              #%.6f表示小数点后带有6位，可根据精确度需要修改；
+        image_name = timestr+ ".jpg" #图像命名：时间戳.jpg
+        cv2.imwrite(save_path + image_name, cv_img)  #保存；
+        cv2.imshow("frame" , cv_img)
+        cv2.waitKey(3)
+    else:
+        pass
+ 
+def displayWebcam():
+    rospy.init_node('save_fig_node', anonymous=True)
+ 
+    # make a video_object and init the video object
+    global count,bridge
+    count = 0
+    bridge = CvBridge()
+    rospy.Subscriber('/d4352/color/image_raw', Image, callback)
+    rospy.spin()
+ 
+if __name__ == '__main__':
+    displayWebcam()
+ 
